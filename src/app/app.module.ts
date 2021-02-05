5675dcfabcd3ecd60b8785fb84dbf69bd82f631a
Alex <alex1mosh@gmail.com>
2021-02-05 23:40:41 +0200

    Update to version 11.1.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
