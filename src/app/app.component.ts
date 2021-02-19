c3eace1f5da37b88f75ca5c1d120636ec76250bf
Alex <alex1mosh@gmail.com>
2021-02-20 00:23:47 +0200

    add gtag2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
+declare var gtag;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Alex Portfolio';
+
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
+    navEndEvents.subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-3DGCPSJQ1R', {
+        page_path: event.urlAfterRedirects,
+      });
+    });
+  }
 }
